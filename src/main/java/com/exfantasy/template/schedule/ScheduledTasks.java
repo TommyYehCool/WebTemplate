c999bf75a11d6f8d2b081dd402e8c55ca9b8efee
TommyYehCool <tommy.yeh1112@gmail.com>
2017-02-09 14:25:01 +0800

    新增 Cron Schedule 範例

--- src/main/java/com/exfantasy/template/schedule/ScheduledTasks.java
+++ src/main/java/com/exfantasy/template/schedule/ScheduledTasks.java
@@ -1,7 +1,6 @@
 package com.exfantasy.template.schedule;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Component;
  * 
  * 參考:
  * 	<a href="https://spring.io/guides/gs/scheduling-tasks/">Spring Boot 內建排程</a>
+ *  <a href="http://stackoverflow.com/questions/26147044/spring-cron-expression-for-every-day-101am">Cron Expression</a>
  * </pre>
  * 
  * @author tommy.feng
@@ -21,7 +21,12 @@ public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 //    @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        System.out.println("The time is now " + dateFormat.format(new Date()));
-    }
+//    public void reportCurrentTime() {
+//        System.out.println("Fix Scheduler running -> The time is now " + dateFormat.format(new Date()));
+//    }
+    
+//    @Scheduled(cron = "0 23 14 9 * ?")
+//    public void testCornSchedule() {
+//    	System.out.println("Cron Scheduler running -> The time is now " + dateFormat.format(new Date()));
+//    }
 }
