c6901b10d69332385057b8d6f48d825162c99d0f
TommyYehCool <tommy.yeh1112@gmail.com>
2017-02-17 11:48:08 +0800

    調整測試程式

--- src/test/java/com/exfantasy/template/test/TestLeetcode.java
+++ src/test/java/com/exfantasy/template/test/TestLeetcode.java
@@ -440,6 +440,43 @@ public class TestLeetcode {
 	
 	/**
 	 * <pre>
+	 * [IMPORTANT]
+	 * 
+	 * Say you have an array for which the ith element is the price of a given stock on day i.
+	 * 
+	 * Design an algorithm to find the maximum profit. 
+	 * 
+	 * You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). 
+	 * 
+	 * However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+	 * </pre>
+	 */
+	@Test
+	public void test_leetcode_122() {
+		int[] prices1 = new int[] {10,20,80};
+		int expectedOutput1 = 70;
+		int output1 = maxProfit(prices1);
+		assertThat(output1).isEqualTo(expectedOutput1);
+		
+		int[] prices2 = new int[] {30,20,90,120,100,200};
+		int expectedOutput2 = 200;
+		int output2 = maxProfit(prices2);
+		assertThat(output2).isEqualTo(expectedOutput2);
+	}
+	
+	private int maxProfit(int[] prices) {
+		int len = prices.length, profit = 0;
+        for (int i = 1; i < len; i++) {
+            // as long as there is a price gap, we gain a profit.
+            if (prices[i] > prices[i - 1]) {
+            	profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+	}
+	
+	/**
+	 * <pre>
 	 * Given an integer n, return the number of trailing zeroes in n!.
 	 *
 	 * Note: Your solution should be in logarithmic time complexity.
