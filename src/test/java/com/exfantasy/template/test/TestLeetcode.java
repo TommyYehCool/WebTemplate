d232c2ef68b55db17fa36e49cadf85aee6abdf57
TommyYehCool <tommy.yeh1112@gmail.com>
2017-02-17 11:40:39 +0800

    調整測試程式

--- src/test/java/com/exfantasy/template/test/TestLeetcode.java
+++ src/test/java/com/exfantasy/template/test/TestLeetcode.java
@@ -895,6 +895,184 @@ public class TestLeetcode {
 	
 	/**
 	 * <pre>
+	 * Design a simplified version of Twitter where users can post tweets, 
+	 * 
+	 * follow/unfollow another user and is able to see the 10 most recent tweets in the user's news feed. 
+	 * 
+	 * Your design should support the following methods:
+	 * 
+	 * postTweet(userId, tweetId): Compose a new tweet.
+	 * getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+	 * follow(followerId, followeeId): Follower follows a followee.
+	 * unfollow(followerId, followeeId): Follower unfollows a followee.
+	 * 
+	 * Example:
+	 * 
+	 * Twitter twitter = new Twitter();
+	 * 
+	 * // User 1 posts a new tweet (id = 5).
+	 * twitter.postTweet(1, 5);
+	 * 
+	 * // User 1's news feed should return a list with 1 tweet id -> [5].
+	 * twitter.getNewsFeed(1);
+	 * 
+	 * // User 1 follows user 2.
+	 * twitter.follow(1, 2);
+	 * 
+	 * // User 2 posts a new tweet (id = 6).
+	 * twitter.postTweet(2, 6);
+	 * 
+	 * // User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+	 * // Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+	 * twitter.getNewsFeed(1);
+	 * 
+	 * // User 1 unfollows user 2.
+	 * twitter.unfollow(1, 2);
+	 * 
+	 * // User 1's news feed should return a list with 1 tweet id -> [5],
+	 * // since user 1 is no longer following user 2.
+	 * twitter.getNewsFeed(1);
+	 * </pre>
+	 */
+	@Test
+	public void test_leetcode_355() {
+		Twitter twitter = new Twitter(); // null
+		twitter.postTweet(1, 5); // null
+		List<Integer> news1 = twitter.getNewsFeed(1); // 5
+		List<Integer> expectedNews1 = Arrays.asList(5);
+		twitter.follow(1, 2); // null
+		twitter.postTweet(2, 6); // null
+		List<Integer> news2 = twitter.getNewsFeed(1); // 6,5
+		List<Integer> expectedNews2 = Arrays.asList(6,5);
+		twitter.unfollow(1, 2); // null
+		twitter.getNewsFeed(1); // 5
+		
+		assertThat(news1).isEqualTo(expectedNews1);
+		assertThat(news2).isEqualTo(expectedNews2);
+	}
+	
+	private class Twitter {
+
+		private int timeStamp = 0;
+	    
+	    // easy to find if user exist
+	    private Map<Integer, User> userMap;
+	    
+	    // Tweet link to next Tweet so that we can save a lot of time
+	    // when we execute getNewsFeed(userId)
+	    private class Tweet {
+	        public int id;
+	        public int time;
+	        public Tweet next;
+	        
+	        public Tweet(int id) {
+	            this.id = id;
+	            time = timeStamp++;
+	            next = null;
+	        }
+	    }
+	    
+	    // OO design so User can follow, unfollow and post itself
+	    public class User {
+	        @SuppressWarnings("unused")
+			public int id;
+	        public Set<Integer> followed;
+	        public Tweet tweet_head;
+	        
+	        public User(int id){
+				this.id = id;
+	            followed = new HashSet<>();
+	            follow(id); // first follow itself
+	            tweet_head = null;
+	        }
+	        
+	        public void follow(int id) {
+	            followed.add(id);
+	        }
+	        
+	        public void unfollow(int id) {
+	            followed.remove(id);
+	        }
+	        
+	        // everytime user post a new tweet, add it to the head of tweet list.
+	        public void post(int id) {
+				Tweet t = new Tweet(id);
+				t.next = tweet_head;
+				tweet_head = t;
+	        }
+	    }
+	    
+	    /** Initialize your data structure here. */
+	    public Twitter() {
+	        userMap = new HashMap<Integer, User>();
+	    }
+	    
+	    /** Compose a new tweet. */
+	    public void postTweet(int userId, int tweetId) {
+			if (!userMap.containsKey(userId)) {
+				User u = new User(userId);
+				userMap.put(userId, u);
+			}
+			userMap.get(userId).post(tweetId);
+		}
+	    
+	    /*
+	     * Best part of this.
+	     * first get all tweets lists from one user including itself and all people it followed.
+	     * Second add all heads into a max heap. Every time we poll a tweet with 
+	     * largest time stamp from the heap, then we add its next tweet into the heap.
+	     * So after adding all heads we only need to add 9 tweets at most into this 
+	     * heap before we get the 10 most recent tweet.
+	     */
+	    public List<Integer> getNewsFeed(int userId) {
+			List<Integer> res = new LinkedList<>();
+
+			if (!userMap.containsKey(userId))
+				return res;
+
+			Set<Integer> users = userMap.get(userId).followed;
+			PriorityQueue<Tweet> q = new PriorityQueue<Tweet>(users.size(), (a, b) -> (b.time - a.time));
+			for (int user : users) {
+				Tweet t = userMap.get(user).tweet_head;
+				// very imporant! If we add null to the head we are screwed.
+				if (t != null) {
+					q.add(t);
+				}
+			}
+			int n = 0;
+			while (!q.isEmpty() && n < 10) {
+				Tweet t = q.poll();
+				res.add(t.id);
+				n++;
+				if (t.next != null)
+					q.add(t.next);
+			}
+			return res;
+	    }
+	    
+	    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+		public void follow(int followerId, int followeeId) {
+			if (!userMap.containsKey(followerId)) {
+				User u = new User(followerId);
+				userMap.put(followerId, u);
+			}
+			if (!userMap.containsKey(followeeId)) {
+				User u = new User(followeeId);
+				userMap.put(followeeId, u);
+			}
+			userMap.get(followerId).follow(followeeId);
+		}
+	    
+	    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+		public void unfollow(int followerId, int followeeId) {
+			if (!userMap.containsKey(followerId) || followerId == followeeId)
+				return;
+			userMap.get(followerId).unfollow(followeeId);
+		}
+	}
+
+	/**
+	 * <pre>
 	 * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
 	 * 
 	 * Example:
