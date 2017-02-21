78ee8d4b42463d0a54d67349675818062785c436
TommyYehCool <tommy.yeh1112@gmail.com>
2017-02-21 14:46:51 +0800

    調整測試程式

--- src/test/java/com/exfantasy/template/test/TestLeetcode.java
+++ src/test/java/com/exfantasy/template/test/TestLeetcode.java
@@ -2564,6 +2564,93 @@ public class TestLeetcode {
 	    }
 	    return res;
 	}
+	
+	/**
+	 * <pre>
+	 * Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+	 * 
+	 * Note:
+	 * The length of num is less than 10002 and will be ≥ k.
+	 * The given num does not contain any leading zero.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: num = "1432219", k = 3
+	 * Output: "1219"
+	 * Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+	 * 
+	 * Example 2:
+	 * 
+	 * Input: num = "10200", k = 1
+	 * Output: "200"
+	 * Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+	 * 
+	 * Example 3:
+	 * 
+	 * Input: num = "10", k = 2
+	 * Output: "0"
+	 * Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+	 * </pre>
+	 */
+	@Test
+	public void test_leetcode_402() {
+		String num1 = "1432219";
+		int k1 = 3;
+		String output1 = removeKdigits(num1, k1);
+		String expectedOutput1 = "1219";
+		assertThat(output1).isEqualTo(expectedOutput1);
+		
+		String num2 = "10200";
+		int k2 = 1;
+		String output2 = removeKdigits(num2, k2);
+		String expectedOutput2 = "200";
+		assertThat(output2).isEqualTo(expectedOutput2);
+		
+		String num3 = "10";
+		int k3 = 2;
+		String output3 = removeKdigits(num3, k3);
+		String expectedOutput3 = "0";
+		assertThat(output3).isEqualTo(expectedOutput3);
+	}
+	
+    private String removeKdigits(String num, int k) {
+		int len = num.length();
+
+		// corner case
+		if (k == len)
+			return "0";
+
+		Stack<Character> stack = new Stack<>();
+		int i = 0;
+		while (i < len) {
+			// whenever meet a digit which is less than the previous digit, discard the previous one
+			while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+				stack.pop();
+				k--;
+			}
+			stack.push(num.charAt(i));
+			i++;
+		}
+
+		// corner case like "1111"
+		while (k > 0) {
+			stack.pop();
+			k--;
+		}
+
+		// construct the number from the stack
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty())
+			sb.append(stack.pop());
+
+		sb.reverse();
+
+		// remove all the 0 at the head
+		while (sb.length() > 1 && sb.charAt(0) == '0')
+			sb.deleteCharAt(0);
+
+		return sb.toString();
+    }
 
 	/**
 	 * <pre>
