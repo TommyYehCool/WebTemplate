aac476d6b55616778b15ad1373da9d86201585ed
TommyYehCool <tommy.yeh1112@gmail.com>
2017-02-21 18:23:47 +0800

    調整測試程式

--- src/test/java/com/exfantasy/template/test/TestLeetcode.java
+++ src/test/java/com/exfantasy/template/test/TestLeetcode.java
@@ -1315,6 +1315,49 @@ public class TestLeetcode {
 		}
 		return A[0];
     }
+    
+    /**
+     * <pre>
+     * [IMPORTANT]
+     * 
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * 
+     * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+     * 
+     * Example 1:
+     * Input: [7, 1, 5, 3, 6, 4]
+     * Output: 5
+     * 
+     * max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+     * 
+     * Example 2:
+     * Input: [7, 6, 4, 3, 1]
+     * Output: 0
+     * 
+     * In this case, no transaction is done, i.e. max profit = 0.
+     * </pre>
+     */
+    @Test
+    public void test_leetcode_121() {
+    	int[] prices1 = new int[] { 7, 1, 5, 3, 6, 4 };
+    	int output1 = maxProfit121(prices1);
+    	int expectedOutput1 = 5;
+    	assertThat(output1).isEqualTo(expectedOutput1);
+    	
+    	int[] prices2 = new int[] { 7, 6, 4, 3, 1 };
+    	int output2 = maxProfit121(prices2);
+    	int expectedOutput2 = 0;
+    	assertThat(output2).isEqualTo(expectedOutput2);
+    }
+    
+    private int maxProfit121(int[] prices) {
+    	int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
 	
 	/**
 	 * <pre>
