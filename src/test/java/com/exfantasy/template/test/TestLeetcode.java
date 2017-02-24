342e99d480a2cead661c4b28ec02d6e5188486a7
TommyYehCool <tommy.yeh1112@gmail.com>
2017-02-24 17:14:14 +0800

    Modify testing code

--- src/test/java/com/exfantasy/template/test/TestLeetcode.java
+++ src/test/java/com/exfantasy/template/test/TestLeetcode.java
@@ -1463,15 +1463,11 @@ public class TestLeetcode {
 		};
 		setZeroes(matrix);
 		
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length; j++) {
-				System.out.print(matrix[i][j]);
-			}
-			System.out.println();
-		}
+		showMatrix(matrix);
+
 		System.out.println("<<<< test_leetcode_73 done");
 	}
-	
+
     private void setZeroes(int[][] matrix) {
 		boolean fr = false, fc = false;
 		for (int i = 0; i < matrix.length; i++) {
@@ -1486,6 +1482,7 @@ public class TestLeetcode {
 				}
 			}
 		}
+		
 		for (int i = 1; i < matrix.length; i++) {
 			for (int j = 1; j < matrix[0].length; j++) {
 				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -1493,17 +1490,30 @@ public class TestLeetcode {
 				}
 			}
 		}
+		
 		if (fr) {
 			for (int j = 0; j < matrix[0].length; j++) {
 				matrix[0][j] = 0;
 			}
 		}
+		
 		if (fc) {
 			for (int i = 0; i < matrix.length; i++) {
 				matrix[i][0] = 0;
 			}
 		}
     }
+    
+    private void showMatrix(int[][] matrix) {
+    	System.out.println("~~~~~~~~~~");
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("~~~~~~~~~~");
+	}
 	
 	/**
 	 * <pre>
