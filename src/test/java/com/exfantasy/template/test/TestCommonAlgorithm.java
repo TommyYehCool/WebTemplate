350e4398bc05838c866662546d9256adde61f285
TommyYehCool <tommy.yeh1112@gmail.com>
2017-03-01 14:02:42 +0800

    Modify testing code

--- src/test/java/com/exfantasy/template/test/TestCommonAlgorithm.java
+++ src/test/java/com/exfantasy/template/test/TestCommonAlgorithm.java
@@ -48,8 +48,8 @@ public class TestCommonAlgorithm {
 	}
 
 	private void bubbleSort(int[] array) {
-		for (int i = array.length - 1; i > 0; --i)
-			for (int j = 0; j < i; ++j)
+		for (int i = array.length - 1; i > 0; i--)
+			for (int j = 0; j < i; j++)
 				if (array[j] > array[j + 1])
 					swap(array, j, j + 1);
 	}
