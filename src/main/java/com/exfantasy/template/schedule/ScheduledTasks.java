0da93182e57cc8e8e7a71076b599c420514f9241
TommyYehCool <tommy.yeh1112@gmail.com>
2017-03-03 16:42:45 +0800

    調整各個排程時間

--- src/main/java/com/exfantasy/template/schedule/ScheduledTasks.java
+++ src/main/java/com/exfantasy/template/schedule/ScheduledTasks.java
@@ -13,8 +13,11 @@ import com.exfantasy.template.services.nba.NBAService;
  * 參考:
  * <a href="https://spring.io/guides/gs/scheduling-tasks/">Spring Boot 內建排程</a>
  * <a href="http://stackoverflow.com/questions/26147044/spring-cron-expression-for-every-day-101am">Cron Expression</a>
+ * 
+ * Cron Expression 格式說明:
+ * 	second, minute, hour, day of month, month, day(s) of week
  *  
- * PS: 
+ * Heroku 筆記: 
  *	放在 heroku 上, 記得要設定 heroku timezone
  *
  *	指令: 
@@ -29,11 +32,14 @@ public class ScheduledTasks {
     @Autowired
     private NBAService nbaService;
     
-    // second, minute, hour, day of month, month, day(s) of week
-    private final String SCHEDULED_TIME = "0 0 8 * * *";
-
-    // for testing
-//    private final String SCHEDULED_TIME = "0 12 16 * * *";
+    // 抓取最新 NBA 隊伍資料時間
+    private final String FETCH_NEWEST_NBA_TEAM_TIME = "0 0 6 * * *";
+    
+    // 抓取最新 NBA 賽程資料時間
+    private final String FETCH_NEWEST_NBA_SCHEDULES_TIME = "0 0 6 * * *";
+    
+    // 抓取最新 NBA 賽事結果時間
+    private final String FETCH_NEWEST_NBA_GAME_RESULTS_TIME = "0 0 12 * * *";
 
 //    @Scheduled(fixedRate = 5000)
 //    public void reportCurrentTime() {
@@ -42,26 +48,26 @@ public class ScheduledTasks {
 //    }
     
     /**
-     * 每天早上 08:00 去抓最新的 NBA 隊伍資料
+     * 每天早上 06:00 去抓最新的 NBA 隊伍資料
      */
-    @Scheduled(cron = SCHEDULED_TIME)
+    @Scheduled(cron = FETCH_NEWEST_NBA_TEAM_TIME)
     public void fetchNewestNBATeamsInformation() {
     	nbaService.fetchNewestNBATeamsInformation();
     }
     
     /**
-     * 每天早上 08:00 去抓最新的 NBA 賽程資料
+     * 每天早上 06:00 去抓最新的 NBA 賽程資料
      */
-    @Scheduled(cron = SCHEDULED_TIME)
+    @Scheduled(cron = FETCH_NEWEST_NBA_SCHEDULES_TIME)
     public void fetchNewestNBASchedules() {
     	nbaService.fetchNewestNBASchedules();
     }
     
     /**
-     * 每天早上 08:00 去抓最新的 NBA 賽事結果
+     * 每天中午 12:00 去抓最新的 NBA 賽事結果
      */
-    @Scheduled(cron = SCHEDULED_TIME)
-    public void fetchNewestNBAGames() {
+    @Scheduled(cron = FETCH_NEWEST_NBA_GAME_RESULTS_TIME)
+    public void fetchNewestNBAGameResults() {
     	nbaService.fetchTodayNBAGameResults();
     }
 }
