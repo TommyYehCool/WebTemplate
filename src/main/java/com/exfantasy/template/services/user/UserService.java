31f97fb8d0f3426923e4ca856c5d684db8909630
TommyYehCool <tommy.yeh1112@gmail.com>
2017-04-14 09:31:09 +0800

    Optimize imports

--- src/main/java/com/exfantasy/template/services/user/UserService.java
+++ src/main/java/com/exfantasy/template/services/user/UserService.java
@@ -3,7 +3,6 @@ package com.exfantasy.template.services.user;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
